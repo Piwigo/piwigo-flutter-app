047f7d7226748f3465fcc8c2ab74a2750fd3f21c
abrenoch <nlclothing@gmail.com>
2022-04-11 22:26:46 -0400

    added tags page

---
+++ lib/views/RootTagViewPage.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:piwigo_ng/api/API.dart';
+import 'package:piwigo_ng/api/TagAPI.dart';
+import 'package:piwigo_ng/constants/SettingsConstants.dart';
+import 'package:piwigo_ng/services/OrientationService.dart';
+import 'package:piwigo_ng/services/upload/Uploader.dart';
+import 'package:piwigo_ng/views/SettingsViewPage.dart';
+
+import 'package:piwigo_ng/views/components/appbars.dart';
+import 'package:piwigo_ng/views/components/dialogs/dialogs.dart';
+import 'package:piwigo_ng/views/components/sidedrawer.dart';
+import 'package:piwigo_ng/views/components/tag_list_item.dart';
+
+class RootTagViewPage extends StatefulWidget {
+  static const String routeName = '/tags';
+  final bool isAdmin;
+
+  const RootTagViewPage({Key key, this.isAdmin = false}) : super(key: key);
+  @override
+  _RootTagViewPageState createState() => _RootTagViewPageState();
+}
+class _RootTagViewPageState extends State<RootTagViewPage> with SingleTickerProviderStateMixin {
+  String _rootCategory;
+  TextEditingController _searchController = TextEditingController();
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _rootCategory = "0";
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      API.uploader = Uploader(context);
+    });
+  }
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData _theme = Theme.of(context);
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      drawer: SideDrawer(view: 'tag'),
+      body: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (context, innerBoxScrolled) => [
+          AppBarExpandable(
+            scrollController: _scrollController,
+            actions: [IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => SettingsPage()),
+                );
+              },
+              icon: Icon(Icons.settings, color: _theme.iconTheme.color),
+            )],
+            title: appStrings(context).tabBar_tags,
+          ),
+        ],
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: FutureBuilder<Map<String,dynamic>>(
+              future: getTags(), // Albums of the list
+              builder: (BuildContext context, AsyncSnapshot albumSnapshot) {
+                if(albumSnapshot.hasData){
+                  if(albumSnapshot.data['stat'] == 'fail') {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(albumSnapshot.data['result']),
+                    ); //appStrings(context).categoryMainEmtpy
+                  }
+                  var tags = albumSnapshot.data['result']['tags'];
+                  int nbPhotos = 0;
+                  // albums.forEach((cat) => nbPhotos+=cat["total_nb_images"]);
+                  // albums.removeWhere((category) => (
+                  //     category["id"].toString() == _rootCategory
+                  // ));
+                  return RefreshIndicator(
+                    displacement: 20,
+                    notificationPredicate: (notification) {
+                      return notification.metrics.atEdge;
+                    },
+                    onRefresh: () {
+                      setState(() {
+                        print("refresh");
+                      });
+                      return Future.delayed(Duration(milliseconds: 1000));
+                    },
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          _tagGrid(tags),
+                          Center(
+                            child: Container(
+                              padding: EdgeInsets.all(10),
+                              child: Text(appStrings(context).imageCount(nbPhotos), style: TextStyle(fontSize: 20, color: _theme.textTheme.bodyText2.color, fontWeight: FontWeight.w300)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }
+          ),
+        ),
+      ),
+      floatingActionButton: widget.isAdmin? FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return CreateCategoryDialog(catId: "0");
+            }
+          ).whenComplete(() {
+            setState(() {
+              print('refresh');
+            });
+          });
+        },
+        child: Icon(Icons.create_new_folder, color: _theme.primaryColorLight, size: 30),
+      ) : Container(),
+    );
+  }
+
+  Widget _tagGrid(dynamic tags) {
+    int albumCrossAxisCount = MediaQuery.of(context).size.width <= Constants.albumMinWidth ? 1
+        : (MediaQuery.of(context).size.width/Constants.albumMinWidth).floor();
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: albumCrossAxisCount,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        childAspectRatio: albumGridAspectRatio(context),
+      ),
+      padding: EdgeInsets.all(10),
+      itemCount: tags.length,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        var tag = tags[index];
+        return TagListItem(tag, isAdmin: widget.isAdmin, onClose: () {
+          setState(() {});
+        });
+      },
+    );
+  }
+}
---
+++ lib/views/components/tag_list_item.dart
@@ -0,0 +1,306 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:piwigo_ng/constants/SettingsConstants.dart';
+import 'package:piwigo_ng/services/OrientationService.dart';
+import 'package:piwigo_ng/views/CategoryViewPage.dart';
+import 'package:piwigo_ng/api/CategoryAPI.dart';
+
+import 'package:piwigo_ng/views/components/dialogs/dialogs.dart';
+import 'package:piwigo_ng/views/components/snackbars.dart';
+import 'package:piwigo_ng/views/components/custom_shapes.dart';
+
+
+// String albumSubCount(dynamic album, context) {
+//   String displayString = appStrings(context).imageCount(album["total_nb_images"]);
+//   if(album["nb_categories"] > 0) {
+//     displayString += ', ${appStrings(context).subAlbumCount(album["nb_categories"])}';
+//   }
+//   return displayString;
+// }
+
+class TagListItem extends StatefulWidget {
+  const TagListItem(this.tag, {Key key, this.isAdmin = false, this.onClose, this.onOpen}) : super(key: key);
+
+  final dynamic tag;
+  final bool isAdmin;
+  final Function() onClose;
+  final Function() onOpen;
+
+  @override
+  _TagListItemState createState() => _TagListItemState();
+}
+class _TagListItemState extends State<TagListItem> {
+
+  void _onEditAlbum() async {
+    // showDialog(
+    //     context: context,
+    //     builder: (BuildContext context) {
+    //       return EditCategoryDialog(
+    //           catId: widget.album['id'],
+    //           catName: widget.album['name'],
+    //           catDesc: widget.album['comment'],
+    //           privacy: widget.album['status'] == 'private' ? true : false
+    //       );
+    //     }
+    // ).whenComplete(() {
+    //   widget.onClose();
+    // });
+  }
+  void _onMoveAlbum() async {
+    // showDialog(
+    //   context: context,
+    //   builder: (BuildContext context) {
+    //     return MoveOrCopyDialog(
+    //       title: appStrings(context).moveCategory,
+    //       subtitle: appStrings(context).moveCategory_select(widget.album['name']),
+    //       catId: widget.tag['id'].toString(),
+    //       catName: widget.tag['name'],
+    //       isImage: false,
+    //       onSelected: (item) async {
+    //         if (await confirmMoveDialog(context,
+    //           content: appStrings(context).moveCategory_message(widget.album['name'], item.name),
+    //         )) {
+    //           var result = await moveCategory(widget.album['id'], item.id);
+    //           if(result['stat'] == 'fail') {
+    //             ScaffoldMessenger.of(context).showSnackBar(
+    //                 errorSnackBar(context, result['result'])
+    //             );
+    //           } else {
+    //             ScaffoldMessenger.of(context).showSnackBar(
+    //                 albumMovedSnackBar(context)
+    //             );
+    //           }
+    //           Navigator.of(context).pop();
+    //         }
+    //       },
+    //     );
+    //   }
+    // ).whenComplete(() {
+    //   widget.onClose();
+    // });
+    // widget.onClose();
+  }
+  void _onDeleteAlbum() async {
+    // if(widget.album["total_nb_images"] > 0) {
+    //   int choice = await confirmDeleteAlbumWithImagesDialog(context,
+    //     content: appStrings(context).deleteCategory_message(widget.album["total_nb_images"], widget.album["name"]),
+    //     count: widget.album["total_nb_images"],
+    //   );
+    //   var result;
+    //   switch(choice) {
+    //     case 0: result = await deleteCategory(widget.album['id'].toString(),
+    //         deletionMode: 'no_delete');
+    //       break;
+    //     case 1: result = await deleteCategory(widget.album['id'].toString(),
+    //         deletionMode: 'delete_orphans');
+    //       break;
+    //     case 2: result = await deleteCategory(widget.album['id'].toString(),
+    //         deletionMode: 'force_delete');
+    //       break;
+    //     default: break;
+    //   }
+    //   widget.onClose();
+    // }
+    // else {
+    //   if (await confirmDeleteDialog(context,
+    //     content: appStrings(context).deleteCategory_message(widget.album["total_nb_images"], widget.album["name"]),
+    //   )) {
+    //     var result = await deleteCategory(widget.album['id'].toString());
+    //     if(result['stat'] == 'fail') {
+    //       ScaffoldMessenger.of(context).showSnackBar(
+    //           errorSnackBar(context, result['result'])
+    //       );
+    //     } else {
+    //       ScaffoldMessenger.of(context).showSnackBar(
+    //           albumDeletedSnackBar(context)
+    //       );
+    //       widget.onClose();
+    //     }
+    //   }
+    // }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // if(widget.onOpen != null) widget.onOpen();
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(builder: (context) => CategoryViewPage(
+        //     title: widget.album["name"],
+        //     category: widget.album["id"].toString(),
+        //     isAdmin: widget.isAdmin,
+        //     nbImages: widget.album["nb_images"],
+        //   )),
+        // ).whenComplete(() {
+        //   widget.onClose();
+        // });
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Slidable(
+          enabled: widget.isAdmin,
+          child: TagListCard(widget.tag, isAdmin: widget.isAdmin),
+          endActionPane: ActionPane(
+            motion: DrawerMotion(),
+            children: [
+              CustomSlidableAction(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                onPressed: (_) {
+                  _onEditAlbum();
+                },
+                child: Center(
+                  child: Icon(Icons.edit, size: 38, color: Colors.white),
+                ),
+              ),
+              CustomSlidableAction(
+                backgroundColor: Color(0xFF4B4B4B),
+                child: Center(
+                  child: Icon(Icons.reply, size: 38, color: Colors.white),
+                ),
+                onPressed: (_) {
+                  _onMoveAlbum();
+                },
+              ),
+              CustomSlidableAction(
+                backgroundColor: Colors.red,
+                child: Center(
+                  child: Icon(Icons.delete, size: 38, color: Colors.white),
+                ),
+                autoClose: true,
+                onPressed: (_) {
+                  _onDeleteAlbum();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TagListCard extends StatelessWidget {
+  const TagListCard(this.tag, {Key key, this.isAdmin = false}) : super(key: key);
+
+  final dynamic tag;
+  final bool isAdmin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // albumThumbnail(context, tag),
+        albumItemSeparator(context),
+        tagInfo(context, tag),
+        Container(
+          height: albumGridItemHeight(context),
+          decoration: BoxDecoration(
+            color: Theme.of(context).backgroundColor,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          child: isAdmin? Center(
+            child: Container(
+              width: 10,
+              height: 60,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Theme.of(context).iconTheme.color),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                ),
+                color: Theme.of(context).iconTheme.color,
+              ),
+            ),
+          ) : Container(width: 8),
+        ),
+      ],
+    );
+  }
+
+  Widget tagInfo(BuildContext context, tag) {
+
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 0, color: Theme.of(context).backgroundColor),
+          color: Theme.of(context).backgroundColor,
+        ),
+        padding: EdgeInsets.all(5),
+        height: albumGridItemHeight(context),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('${tag["name"]}',
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: false,
+                  maxLines: 1
+              ),
+              Column(
+                children: [
+                  // Text('${album["comment"]}',
+                  //     style: Theme.of(context).textTheme.subtitle1,
+                  //     textAlign: TextAlign.center,
+                  //     overflow: TextOverflow.fade,
+                  //     softWrap: true,
+                  //     maxLines: 2
+                  // ),
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    child: Text(tag["counter"].toString(),
+                      style: Theme.of(context).textTheme.bodyText2,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ],
+              ),
+            ]
+        ),
+      ),
+    );
+  }
+  // Widget albumThumbnail(BuildContext context, album) {
+  //   return LayoutBuilder(builder: (context, layout) {
+  //     return Container(
+  //       decoration: BoxDecoration(
+  //         border: Border.all(width: 0, color: Theme.of(context).backgroundColor),
+  //         color: Theme.of(context).backgroundColor,
+  //         borderRadius: BorderRadius.only(
+  //           topLeft: Radius.circular(10),
+  //           bottomLeft: Radius.circular(10),
+  //         ),
+  //       ),
+  //       padding: EdgeInsets.all(5),
+  //       height: layout.maxHeight,// albumGridItemHeight(context),
+  //       width: layout.maxHeight,// albumGridItemHeight(context),
+  //       child: album["tn_url"] == null ?
+  //       Icon(Icons.image_not_supported_outlined, size: 50)
+  //           :
+  //       ClipRRect(
+  //         borderRadius: BorderRadius.circular(7.0),
+  //         child: Image.network(
+  //           album["tn_url"],
+  //           fit: BoxFit.cover,
+  //         ),
+  //       ),
+  //     );
+  //   });
+  // }
+  Widget albumItemSeparator(BuildContext context) {
+    return Container(
+      decoration: ShapeDecoration(
+        shape: AlbumCardSeparatorShape(radius: 7),
+        color: Theme.of(context).backgroundColor,
+      ),
+      width: 14,
+      height: albumGridItemHeight(context),
+    );
+  }
+}
+
