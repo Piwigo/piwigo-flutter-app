92a393742825850785b2a8f265eef842acdbe462
abrenoch <nlclothing@gmail.com>
2022-04-12 22:42:08 -0400

    added PageArguments.dart

---
+++ lib/model/PageArguments.dart
@@ -0,0 +1,11 @@
+class PageArguments {
+  final bool isAdmin;
+  final String tag;
+  final String category;
+  final String title;
+  final int index;
+  final List<dynamic> images;
+  PageArguments({
+    this.isAdmin, this.tag, this.category, this.title, this.index, this.images
+  });
+}
