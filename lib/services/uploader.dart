fe46840c5173cb3493bdc9830d98403df89965f4
Rémi <remi30150@gmail.com>
2022-10-18 14:32:33 +0200

    added signature and search

--- lib/services/uploader.dart
+++ lib/services/uploader.dart
@@ -5,11 +5,38 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:piwigo_ng/api/api_client.dart';
+import 'package:piwigo_ng/api/authentication.dart';
+import 'package:piwigo_ng/app.dart';
+import 'package:piwigo_ng/services/shared_preferences_service.dart';
+import 'package:piwigo_ng/utils/localizations.dart';
 
 import 'chunked_uploader.dart';
+import 'notification_service.dart';
+
+Future<void> _showUploadNotification({bool success = true}) async {
+  if (!(appPreferences.getBool('upload_notification') ?? true)) return;
+  final android = AndroidNotificationDetails(
+    'piwigo-ng-upload',
+    'Piwigo NG Upload',
+    channelDescription: 'piwigo-ng',
+    priority: Priority.high,
+    importance: Importance.high,
+  );
+  final platform = NotificationDetails(android: android);
+  await localNotification.show(
+    1,
+    success ? 'Success' : 'Failure',
+    success
+        ? appStrings(App.appKey.currentContext!).imageUploadCompleted_message
+        : appStrings(App.appKey.currentContext!).uploadError_message,
+    platform,
+  );
+}
 
 Future<List<Map<String, dynamic>>> uploadPhotos(
   List<XFile> photos,
@@ -51,12 +78,17 @@ Future<List<Map<String, dynamic>>> uploadPhotos(
         }
       }
     }
+    _showUploadNotification(success: true);
   } on DioError catch (e) {
+    _showUploadNotification(success: false);
     debugPrint(e.message);
   }
 
   try {
-    await uploadCompleted(uploadCompletedList);
+    await uploadCompleted(uploadCompletedList, int.parse(category));
+    if (await methodExist('community.images.uploadCompleted')) {
+      await communityUploadCompleted(uploadCompletedList, int.parse(category));
+    }
   } on DioError catch (e) {
     debugPrint(e.message);
   }
@@ -115,7 +147,51 @@ Future<File?> compressFile(XFile file) async {
   return result;
 }
 
-uploadCompleted(List<int> imageIdList) async {}
+Future<Map<String, dynamic>> uploadCompleted(List<int> imageId, int categoryId) async {
+  Map<String, String> queries = {
+    "format": "json",
+    "method": "pwg.images.uploadCompleted",
+  };
+  FormData formData = FormData.fromMap({
+    "image_id": imageId,
+    "pwg_token": appPreferences.getString("pwg_token"),
+    "category_id": categoryId,
+  });
+  try {
+    Response response = await ApiClient.post(data: formData, queryParameters: queries);
+    if (response.statusCode == 200) {
+      return json.decode(response.data);
+    } else {
+      return {'stat': 'fail', 'result': response.statusMessage};
+    }
+  } catch (e) {
+    var error = e as DioError;
+    return {'stat': 'fail', 'result': error.message};
+  }
+}
+
+Future<Map<String, dynamic>> communityUploadCompleted(List<int> imageId, int categoryId) async {
+  Map<String, String> queries = {
+    "format": "json",
+    "method": "community.images.uploadCompleted",
+  };
+  FormData formData = FormData.fromMap({
+    "image_id": imageId,
+    "pwg_token": appPreferences.getString("pwg_token"),
+    "category_id": categoryId,
+  });
+  try {
+    Response response = await ApiClient.post(data: formData, queryParameters: queries);
+    if (response.statusCode == 200) {
+      return json.decode(response.data);
+    } else {
+      return {'stat': 'fail', 'result': response.statusMessage};
+    }
+  } catch (e) {
+    var error = e as DioError;
+    return {'stat': 'fail', 'result': error.message};
+  }
+}
 
 class FlutterAbsolutePath {
   static const MethodChannel _channel = MethodChannel('flutter_absolute_path');
