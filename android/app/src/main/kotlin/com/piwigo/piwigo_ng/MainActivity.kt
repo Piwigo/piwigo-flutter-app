ad52d7e6a54fc8182bd0c420ec6209084cc38a8c
Rémi <remi30150@gmail.com>
2022-10-19 18:58:35 +0200

    fixed java code in kotlin file and overlay

--- android/app/src/main/kotlin/com/piwigo/piwigo_ng/MainActivity.kt
+++ android/app/src/main/kotlin/com/piwigo/piwigo_ng/MainActivity.kt
@@ -3,12 +3,4 @@ package com.remi.piwigo_ng
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onPostResume() {
-        super.onPostResume()
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-        }
-    }
 }
