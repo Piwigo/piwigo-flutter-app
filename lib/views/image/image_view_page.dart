971a39b8277cece5ee3e58a580766516c8b38382
Rémi <remi30150@gmail.com>
2023-05-09 19:39:17 +0200

    Added comment dialog

--- lib/views/image/image_view_page.dart
+++ lib/views/image/image_view_page.dart
@@ -18,6 +18,7 @@ import 'package:piwigo_ng/services/preferences_service.dart';
 import 'package:piwigo_ng/utils/image_actions.dart';
 import 'package:piwigo_ng/utils/localizations.dart';
 import 'package:piwigo_ng/utils/resources.dart';
+import 'package:piwigo_ng/utils/settings.dart';
 import 'package:piwigo_ng/views/image/video_player_page.dart';
 
 /// Media Full Screen page
@@ -632,5 +633,140 @@ class _ImageViewPageState extends State<ImageViewPage> {
     ];
 
     return widget.isAdmin ? adminActions : userActions;
+  }
+}
+
+// todo: create component file "image_comment_dialog.dart"
+class ImageCommentDialog extends StatefulWidget {
+  const ImageCommentDialog({Key? key, required this.image}) : super(key: key);
+
+  final ImageModel image;
+
+  @override
+  State<ImageCommentDialog> createState() => _ImageCommentDialogState();
+}
+
+class _ImageCommentDialogState extends State<ImageCommentDialog> {
+  final ScrollController _controller = ScrollController();
+
+  @override
+  void initState() {
+    _controller.addListener(() => setState(() {}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isScrolled = false;
+    bool isNameHidden = false;
+    if (_controller.positions.isNotEmpty) {
+      isScrolled = _controller.offset > 0.0;
+      isNameHidden = _controller.offset > kToolbarHeight / 2;
+    }
+    return AlertDialog(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      contentPadding: EdgeInsets.zero,
+      insetPadding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+      titlePadding: EdgeInsets.zero,
+      title: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+        ),
+        primary: false,
+        elevation: isScrolled ? 5.0 : 0.0,
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(Icons.close),
+        ),
+        title: AnimatedOpacity(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.ease,
+          opacity: isNameHidden ? 1.0 : 0.0,
+          child: Text(
+            widget.image.name,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+      ),
+      content: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: Settings.modalMaxWidth,
+        ),
+        child: SingleChildScrollView(
+          controller: _controller,
+          padding: EdgeInsets.symmetric(
+            vertical: 16.0,
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                widget.image.name,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Divider(
+                color: Theme.of(context).disabledColor,
+              ),
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempor, eros eget ultrices tincidunt, lacus quam tempor neque, eu venenatis tellus nulla sit amet lectus. Praesent lacus orci, fermentum a elit vitae, cursus iaculis sapien. Sed ac ipsum ante. In efficitur urna sed pulvinar accumsan. Aliquam et condimentum ex. Quisque eleifend ipsum vitae magna gravida, eget maximus nisi posuere. Maecenas a dui et est posuere sollicitudin ac eget tellus. In quis est purus. Cras eget vestibulum justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vel elit in sapien bibendum porta. In eget felis auctor, tincidunt lectus sed, placerat lectus. Vivamus ac mauris sit amet turpis mollis tempor.Donec metus nunc, ultricies id lobortis sed, accumsan varius nunc. Duis et posuere ex. Donec ante urna, ornare non elit ac, egestas condimentum ex. Curabitur aliquam nec purus a hendrerit. Nam metus massa, aliquam eu ligula a, pretium maximus orci. Morbi eros arcu, egestas et lorem et, mattis posuere ex. In at lacus erat. Mauris aliquam lectus eget dui pretium faucibus. Ut mollis elit at orci tempor rutrum. Fusce tincidunt libero nisi, nec efficitur odio laoreet in. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam consequat massa feugiat, rutrum eros sed, blandit nunc. Duis ac eleifend turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam malesuada scelerisque purus vitae auctor. Etiam ut rhoncus eros.Pellentesque eget velit vel massa ultricies vestibulum quis in orci. Vivamus porttitor libero at elit dignissim, ut iaculis lorem placerat. Vivamus sed nisl turpis. Sed dictum enim a rhoncus lacinia. Pellentesque non finibus massa. Aenean leo diam, elementum et turpis a, placerat laoreet libero. Proin eu euismod felis, at laoreet augue. Vestibulum lobortis sapien ac scelerisque hendrerit.Etiam lacinia tempus erat, sed semper turpis ultrices ut. Aliquam erat volutpat. Aenean semper venenatis rhoncus. Cras sollicitudin ullamcorper ante, id placerat lacus facilisis id. Donec a metus consequat, accumsan ipsum a, tincidunt mi. Donec ullamcorper rhoncus augue, sed dapibus urna. Ut sed interdum arcu. Aliquam sagittis pulvinar feugiat. Maecenas sed auctor lectus, quis dapibus nisi. Etiam ac ullamcorper nulla, eget porttitor eros. Mauris varius lorem magna, eget ultrices lacus vestibulum in. Sed magna nulla, bibendum et orci et, placerat ultricies orci. Morbi consequat arcu odio, eget tristique purus porttitor non. Mauris ac egestas dolor. Nulla nec aliquam quam. Suspendisse consectetur ex non elementum maximus.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas accumsan ante mattis nisi maximus, in feugiat metus pretium. Nam at porttitor purus, a cursus dolor. Etiam luctus eleifend hendrerit. Phasellus at mauris vulputate, volutpat neque vitae, laoreet massa. Nullam feugiat felis nec mi feugiat mollis. Phasellus consequat luctus dictum. Aliquam erat volutpat. In hac habitasse platea dictumst. Donec fringilla eleifend lorem. Aliquam erat volutpat. Pellentesque in elit vitae arcu maximus viverra. Donec accumsan lectus non bibendum pretium.",
+                // image.comment!,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    return Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: Settings.modalMaxWidth,
+        ),
+        child: GestureDetector(
+          onTap: () => Navigator.of(context).pop(),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            padding: EdgeInsets.symmetric(
+              vertical: 32.0,
+              horizontal: 32.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  widget.image.name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+                Divider(
+                  color: Colors.white,
+                ),
+                Text(
+                  // "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempor, eros eget ultrices tincidunt, lacus quam tempor neque, eu venenatis tellus nulla sit amet lectus. Praesent lacus orci, fermentum a elit vitae, cursus iaculis sapien. Sed ac ipsum ante. In efficitur urna sed pulvinar accumsan. Aliquam et condimentum ex. Quisque eleifend ipsum vitae magna gravida, eget maximus nisi posuere. Maecenas a dui et est posuere sollicitudin ac eget tellus. In quis est purus. Cras eget vestibulum justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vel elit in sapien bibendum porta. In eget felis auctor, tincidunt lectus sed, placerat lectus. Vivamus ac mauris sit amet turpis mollis tempor.Donec metus nunc, ultricies id lobortis sed, accumsan varius nunc. Duis et posuere ex. Donec ante urna, ornare non elit ac, egestas condimentum ex. Curabitur aliquam nec purus a hendrerit. Nam metus massa, aliquam eu ligula a, pretium maximus orci. Morbi eros arcu, egestas et lorem et, mattis posuere ex. In at lacus erat. Mauris aliquam lectus eget dui pretium faucibus. Ut mollis elit at orci tempor rutrum. Fusce tincidunt libero nisi, nec efficitur odio laoreet in. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam consequat massa feugiat, rutrum eros sed, blandit nunc. Duis ac eleifend turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam malesuada scelerisque purus vitae auctor. Etiam ut rhoncus eros.Pellentesque eget velit vel massa ultricies vestibulum quis in orci. Vivamus porttitor libero at elit dignissim, ut iaculis lorem placerat. Vivamus sed nisl turpis. Sed dictum enim a rhoncus lacinia. Pellentesque non finibus massa. Aenean leo diam, elementum et turpis a, placerat laoreet libero. Proin eu euismod felis, at laoreet augue. Vestibulum lobortis sapien ac scelerisque hendrerit.Etiam lacinia tempus erat, sed semper turpis ultrices ut. Aliquam erat volutpat. Aenean semper venenatis rhoncus. Cras sollicitudin ullamcorper ante, id placerat lacus facilisis id. Donec a metus consequat, accumsan ipsum a, tincidunt mi. Donec ullamcorper rhoncus augue, sed dapibus urna. Ut sed interdum arcu. Aliquam sagittis pulvinar feugiat. Maecenas sed auctor lectus, quis dapibus nisi. Etiam ac ullamcorper nulla, eget porttitor eros. Mauris varius lorem magna, eget ultrices lacus vestibulum in. Sed magna nulla, bibendum et orci et, placerat ultricies orci. Morbi consequat arcu odio, eget tristique purus porttitor non. Mauris ac egestas dolor. Nulla nec aliquam quam. Suspendisse consectetur ex non elementum maximus.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas accumsan ante mattis nisi maximus, in feugiat metus pretium. Nam at porttitor purus, a cursus dolor. Etiam luctus eleifend hendrerit. Phasellus at mauris vulputate, volutpat neque vitae, laoreet massa. Nullam feugiat felis nec mi feugiat mollis. Phasellus consequat luctus dictum. Aliquam erat volutpat. In hac habitasse platea dictumst. Donec fringilla eleifend lorem. Aliquam erat volutpat. Pellentesque in elit vitae arcu maximus viverra. Donec accumsan lectus non bibendum pretium.",
+                  widget.image.comment!,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
