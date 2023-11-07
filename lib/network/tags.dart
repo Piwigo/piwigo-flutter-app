81f87722ec3dabe31bf8ff333995c80c4f19e780
abrenoch <nlclothing@gmail.com>
2023-11-06 22:46:32 -0500

    added editTag to tags.dart

--- lib/network/tags.dart
+++ lib/network/tags.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:piwigo_ng/models/tag_model.dart';
 import 'package:piwigo_ng/network/api_error.dart';
+import 'package:piwigo_ng/services/preferences_service.dart';
 
 import 'api_client.dart';
 
@@ -57,6 +58,37 @@ Future<ApiResponse<TagModel>> createTag(String name) async {
     debugPrint('Fetch tags: ${e.message}');
   } on Error catch (e) {
     debugPrint('Fetch tags: $e\n${e.stackTrace}');
+  }
+  return ApiResponse(error: ApiErrors.error);
+}
+
+Future<dynamic> editTag(int tagId, String tagName) async {
+  Map<String, String> queries = {
+    "format": "json",
+    "method": "pwg.tags.rename",
+  };
+  FormData formData = FormData.fromMap({
+    "tag_id": tagId,
+    "new_name": tagName,
+    'pwg_token': appPreferences.getString('PWG_TOKEN'),
+  });
+  Response response = await ApiClient.post(
+      data: formData,
+      queryParameters: queries
+  );
+
+  try {
+    if (response.statusCode == 200) {
+      var data = json.decode(response.data);
+      if (data['stat'] == 'fail') {
+        return ApiResponse(error: ApiErrors.error);
+      }
+      return ApiResponse(data: true);
+    }
+  } on DioError catch (e) {
+    debugPrint('Get tags: ${e.message}');
+  } on Error catch (e) {
+    debugPrint('Get tags: $e\n${e.stackTrace}');
   }
   return ApiResponse(error: ApiErrors.error);
 }
