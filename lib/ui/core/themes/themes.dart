501e31338c7afafaa6e2365cac6abbcf3ff9886a
RushLana <renarde-dev@la-taniere-solidaire.fr>
2025-10-22 15:07:30 +0200

    Import existing colors and slowly reimplement themes
    This will require a lot of work to deduplicate and separate dimensions and theme data

---
+++ lib/ui/core/themes/themes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+final class AppTheme {
+  // Use default material theme as a base
+  static ThemeData lightTheme = ThemeData.light(useMaterial3: true).copyWith(
+
+  );
+
+  static ThemeData darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
+
+  );
+}
