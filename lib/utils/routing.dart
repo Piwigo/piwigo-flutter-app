28f5b63ece86efe7ef9031cdcfc928e6f58334a1
RushLana <renarde-dev@la-taniere-solidaire.fr>
2025-10-30 14:44:32 +0100

    Update routing and add todo for navigator migration

--- lib/utils/routing.dart
+++ lib/utils/routing.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:piwigo_ng/ui/album/album_screen.dart';
-import 'package:piwigo_ng/ui/authentification/instance_screen.dart';
+import 'package:piwigo_ng/ui/authentification/widgets/instance_screen.dart';
 
 //Temporary
 var isLogged = false;
+// TODO Need to switch navigator calls in screens to https://pub.dev/documentation/go_router/latest/topics/Configuration-topic.html
 
 // GoRouter configuration
 final router = GoRouter(
@@ -17,6 +18,11 @@ final router = GoRouter(
     GoRoute(
       path: '/login',
       builder: (context, state) => InstanceScreen(),
+      routes: [
+        GoRoute(
+            path: ''
+        )
+      ]
     )
   ],
   redirect: (BuildContext context, GoRouterState state) {
